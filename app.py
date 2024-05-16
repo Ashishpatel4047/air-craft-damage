a93a7ac874f61784dbc99d65ce983dbe7eb2edc9
ashish patel <117652257+Ashishpatel4047@users.noreply.github.com>
2024-05-16 10:28:19 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,56 @@
-import pickle
-from flask import Flask,request,app,jsonify,url_for,redirect,flash,session,render_template
-
-import numpy as np
+import streamlit as st
 import pandas as pd
+import joblib
+import numpy as np
 
-app=Flask(__name__)
-## Lodd the model
-model=pickle.load(open("Aircraft_Damage_Propagation.pkl",'rb'))
-@app.route('/')
-def home():
-    return render_template("index.html")
-
-@app.route('/predict_api',methods=['POST'])
-
-def predict_api():
-   data=request.json['data']
-   print(data)
-   print(np.array(list(data.values()))).reshape(1,-1)
-   new_data=np.ScalarType.transform(np.array (list(data.values())).reshape(1,-1))
-   output=model.predict(new_data)
-   return jsonify(output[0])
+# Load the pre-trained model
+@st.cache(allow_output_mutation=True)
+def load_model():
+    return joblib.load('Aircraft Damage Propagation (1).pkl')
 
 
+# Main function to run the app
+def main():
+    st.title('Aircraft Prediction')
+    # Load the pre-trained model
+    model = load_model()
 
-if __name__=="__main__":
-    app.debug(True)
- 
- 
+    # Prediction form
+    st.subheader('Make a Prediction')
+    unit_number = st.number_input('unit_number', min_value=0.0)
+    time_cycles = st.number_input('time_cycles', min_value=0.0)
+    setting_1 = st.number_input('setting_1', min_value=0.0)
+    setting_2 = st.number_input('setting_2', min_value=0.0)
+    setting_3 = st.number_input('setting_3', min_value=0.0)
+    sensor_1 = st.number_input('s_1', min_value=0.0)
+    sensor_2 = st.number_input('s_2', min_value=0.0)
+    sensor_3 = st.number_input('s_3', min_value=0.0)
+    sensor_4 = st.number_input('s_4', min_value=0.0)
+    sensor_5 = st.number_input('s_5', min_value=0.0)
+    sensor_6 = st.number_input('s_6', min_value=0.0)
+    sensor_7 = st.number_input('s_7', min_value=0.0)
+    sensor_8 = st.number_input('s_8', min_value=0.0)
+    sensor_9 = st.number_input('s_9', min_value=0.0)
+    sensor_10 = st.number_input('s_10', min_value=0.0)
+    sensor_11 = st.number_input('s_11', min_value=0.0)
+    sensor_12 = st.number_input('s_12', min_value=0.0)
+    sensor_13 = st.number_input('s_13', min_value=0.0)
+    sensor_14 = st.number_input('s_14', min_value=0.0)
+    sensor_15 = st.number_input('s_15', min_value=0.0)
+    sensor_16 = st.number_input('s_16', min_value=0.0)
+    sensor_17 = st.number_input('s_17', min_value=0.0)
+    sensor_18 = st.number_input('s_18', min_value=0.0)
+    sensor_19 = st.number_input('s_19', min_value=0.0)
+    sensor_20 = st.number_input('s_20', min_value=0.0)
+    sensor_21 = st.number_input('s_21', min_value=0.0)
+
+    if st.button('Predict'):
+        prediction = model.predict([[unit_number, time_cycles, setting_1, setting_2, setting_3,
+                                     sensor_1, sensor_2, sensor_3, sensor_4, sensor_5, sensor_6, sensor_7, sensor_8, sensor_9,
+                                     sensor_10, sensor_11, sensor_12, sensor_13, sensor_14, sensor_15, sensor_16, sensor_17, sensor_18,
+                                     sensor_19, sensor_20, sensor_21]])
+        st.success(f'Aircraft prediction: {prediction[0]}')
+
+
+if __name__ == '__main__':
+    main()
